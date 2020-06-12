c0853d160fb6b6fe050faf0b0c3a3a4c73076d6b
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-06-12 09:53:53 -0700

    Ring Buffer seems to be passing! I am happy.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,12 +2,20 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = []
+        self.oldestItem = 0
 
 
     def append(self, item):
-        self.storage.append(item)
-        if len(self.storage) == self.capacity:
-            self.cur = 0
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.oldestItem] = item
+            if self.oldestItem < len(self.storage) -1:
+                self.oldestItem += 1
+            else:
+                self.oldestItem = 0
+
+            # self.cur = (self.cur+1) % self.capacity
 
     def get(self):
         return self.storage
